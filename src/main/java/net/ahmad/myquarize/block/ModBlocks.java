52c97b466991a709dca14b27f4cfcc8a32b51cb9
Ahmad0126 <azaid7492@gmail.com>
2024-01-07 16:22:07 +0700

    Loot Table

--- src/main/java/net/ahmad/myquarize/block/ModBlocks.java
+++ src/main/java/net/ahmad/myquarize/block/ModBlocks.java
@@ -5,19 +5,21 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
-        new Block(FabricBlockSettings.copyOf(Blocks.EMERALD_BLOCK).strength(0.2f)));
+        new Block(FabricBlockSettings.copyOf(Blocks.EMERALD_BLOCK)));
     public static final Block RUBY_ORE = registerBlock("ruby_ore",
-        new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(0.1f)));
+        new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.IRON_ORE), UniformIntProvider.create(2, 5)));
     public static final Block STEEL_BLOCK = registerBlock("steel_block",
-        new Block(FabricBlockSettings.copyOf(Blocks.NETHERITE_BLOCK).strength(0.4f)));
+        new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).strength(10f, 12f)));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
